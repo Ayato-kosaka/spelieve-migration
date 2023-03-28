2a7fb637e7ab5393c2b4f13146f32865ced5a51e
Ayato-kosaka <kosaka.ayato@gmail.com>
2023-03-28 13:02:18 +0000

    The default Firebase app already exists. This means you called initializeApp() more than once
    のエラー対応

--- src/developmentDB.ts
+++ src/developmentDB.ts
@@ -2,12 +2,15 @@ import * as admin from "firebase-admin";
 import dotenv from "dotenv";
 
 dotenv.config({ path: "./spelieve-secret/spelieve-backend/development/.env" });
-admin.initializeApp({
-  credential: admin.credential.cert({
-    projectId: process.env.FIREBASE_PROJECT_ID!,
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL!,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY!.replace(/\\n/g, "\n"),
-  }),
-});
+admin.initializeApp(
+  {
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID!,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL!,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY!.replace(/\\n/g, "\n"),
+    }),
+  },
+  admin.apps.length > 0 ? "development" : undefined
+);
 
 export const developmentDB = admin.firestore();
--- src/productionDB.ts
+++ src/productionDB.ts
@@ -2,12 +2,15 @@ import * as admin from "firebase-admin";
 import dotenv from "dotenv";
 
 dotenv.config({ path: "./spelieve-secret/spelieve-backend/production/.env" });
-admin.initializeApp({
-  credential: admin.credential.cert({
-    projectId: process.env.FIREBASE_PROJECT_ID!,
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL!,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY!.replace(/\\n/g, "\n"),
-  }),
-});
+admin.initializeApp(
+  {
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID!,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL!,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY!.replace(/\\n/g, "\n"),
+    }),
+  },
+  admin.apps.length > 0 ? "production" : undefined
+);
 
 export const productionDB = admin.firestore();
